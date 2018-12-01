eb439720a2a59ee52c892c6f4d2fef062470c4b9
ZararAhmed <zarar1471@gmail.com>
2018-12-01 22:52:53 +0500

    realtime user data from fire-db

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 var config = {
     apiKey: "AIzaSyATaBfK4dmCtK6vySq7xvSvunQonoVos4Q",
     authDomain: "web-demo-ca2d3.firebaseapp.com",
@@ -12,14 +13,18 @@ var config = {
       constructor() {
           app.initializeApp(config);
           this.auth = app.auth();
+          this.db = app.database();
       }
-
-        // *** Authentication API ***
-        
+        // *** Authentication API *** 
         doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
         doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
         doSignOut = () => this.auth.signOut();
         doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
         doPasswordUpdate = password =>this.auth.currentUser.updatePassword(password);
+
+        // *** User API ***
+
+        user = uid => this.db.ref(`users/${uid}`);
+        users = () => this.db.ref('users/');
   }
   export default Firebase;
