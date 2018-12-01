625840c1fbf38f239935efcf7bb43d984480d27f
ZararAhmed <zarar1471@gmail.com>
2018-12-01 21:10:50 +0500

    authorization and protected routes

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-const Home = () => (
-      <div>
-          Account
-      </div>
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+import { withAuthorization } from '../Session';
+
+const AccountPage = () => (
+  <div>
+    {/* <h1>Account: {authUser.email}</h1> */}
+    <PasswordForgetForm />
+    <PasswordChangeForm />
+  </div>
 );
 
-export default Home;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);   
